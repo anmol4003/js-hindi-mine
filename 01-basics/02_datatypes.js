397db62a14a5a2a01a2017c44db21aaabca09e9b
anmol4003 <145591420+anmol4003@users.noreply.github.com>
2023-09-20 17:30:05 +0000

    datatypes

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version(also automatic)
+
+// alert (3+3) // we are using nodejs, not browser it willl not work.
+
+let name = "anmol" // string datatype
+let age = 18 // number datatype
+let isLoggedIn = false // boolean datatype
+let state; //undefined
+
+//    PRIMITIVE DATATYPE     //
+// number datatype range => 2 to power 53
+// bigint (big numbers)
+// string => ""
+// boolean = true or false
+
+// undefined =>  // it have its own type //
+// symbol => unique
+
+
+
+//                Object DATATYPE             //
+// null(datatype) => stand alone value  
+
+
+console.log(typeof "anmol");
