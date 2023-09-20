507511fdedcd7074bb4634a5278fb47bd0d04f0b
anmol4003 <145591420+anmol4003@users.noreply.github.com>
2023-09-20 17:29:50 +0000

    variables

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "vermaji4003@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+let accountState;
+// accountId = 2
+console.log(accountId);
+console.table([accountId, accountEmail , accountPassword, accountCity, accountState])
