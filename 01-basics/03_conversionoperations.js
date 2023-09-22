063bc1cf85f0e61ee222109dfd3f3405e3de043e
anmol4003 <145591420+anmol4003@users.noreply.github.com>
2023-09-22 11:04:42 +0000

    conversion of operations

--- 01-basics/03_conversionoperations.js
+++ 01-basics/03_conversionoperations.js
@@ -1,11 +1,11 @@
 let score = "33abh" // if we take "null" then we get 0 in (valueInNumber), 
 //and also if we take "undefined" then we get "nan" 
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abv" => nan not a number
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = "0"
 let valueInBoolean = Boolean(isLoggedIn);
-console.log(valueInBoolean);
+//console.log(valueInBoolean);
 
 //1 => true; 0=> false
 // "" => false
@@ -23,5 +23,45 @@ console.log(valueInBoolean);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log( stringNumber);
-console.log(typeof stringNumber);
+// console.log( stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ++++++++++++++++++++++++++++  operations +++++++++++++++++++++++++++++++++++++++++++++++++
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+
+// console.log(2-2);   // subtraction
+// console.log(2+2);  // addition
+// console.log(2+2);  // multiplication
+// console.log(2**2); //power
+// console.log(2/2); //division
+// console.log(2%2); //remainder
+ 
+
+let str1 = "hello"
+let str2 = " Anmol"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4)*5%3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+
+let gameCounter = 100
+gameCounter++;//prefix and postfix counter => ++abc   , abc++
+console.log(gameCounter);
